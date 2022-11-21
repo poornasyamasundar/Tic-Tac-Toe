b626bc1ca11acbca4bdf5552918d359f68bb450c
poornasyamasundar <70936332+poornasyamasundar@users.noreply.github.com>
2022-11-21 12:03:29 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -116,7 +116,7 @@ def get_paramgrid_rf():
   # write your code here...
   return rf_param_grid
 
-def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
+def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
   
   # you need to invoke sklearn grid search cv function
   # refer to sklearn documentation
@@ -128,12 +128,12 @@ def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None,
 
   grid_search_cv = None
   
-  grid_search_cv = GridSearchCV(model, param_grid, scoring = metrics[0])
+  grid_search_cv = GridSearchCV(model1, param_grid, scoring = metrics[0])
   grid_search_cv.fit(X,y)
   
   top1_scores.append(grid_search_cv.best_score_)
 
-  grid_search_cv = GridSearchCV(model, param_grid, scoring = metrics[1])
+  grid_search_cv = GridSearchCV(model1, param_grid, scoring = metrics[1])
   grid_search_cv.fit(X,y)
   
   top1_scores.append(grid_search_cv.best_score_)
